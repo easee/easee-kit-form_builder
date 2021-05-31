bd75ca9b70f9a0b969b75f9da69b82d118500956
Alek Slater <slayte@gmail.com>
2021-05-31 15:20:57 +0200

    hopefully this fixes weird issues

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -130,53 +130,53 @@ class MyHomePageState extends State<MyHomePage> {
                       colorPickerType: ColorPickerType.SlidePicker,
                       decoration: InputDecoration(labelText: "Pick Color"),
                     ),
-                    FormBuilderChipsInput(
-                      decoration: InputDecoration(labelText: "Chips"),
-                      attribute: 'chips_test',
-                      onChanged: _onChanged,
-                      initialValue: [
-                        Contact('Andrew', 'stock@man.com',
-                            'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4057996.jpg'),
-                      ],
-                      maxChips: 5,
-                      findSuggestions: (String query) {
-                        if (query.length != 0) {
-                          var lowercaseQuery = query.toLowerCase();
-                          return contacts.where((profile) {
-                            return profile.name.toLowerCase().contains(query.toLowerCase()) ||
-                                profile.email.toLowerCase().contains(query.toLowerCase());
-                          }).toList(growable: false)
-                            ..sort((a, b) => a.name
-                                .toLowerCase()
-                                .indexOf(lowercaseQuery)
-                                .compareTo(b.name.toLowerCase().indexOf(lowercaseQuery)));
-                        } else {
-                          return const <Contact>[];
-                        }
-                      },
-                      chipBuilder: (context, state, profile) {
-                        return InputChip(
-                          key: ObjectKey(profile),
-                          label: Text(profile.name),
-                          avatar: CircleAvatar(
-                            backgroundImage: NetworkImage(profile.imageUrl),
-                          ),
-                          onDeleted: () => state.deleteChip(profile),
-                          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                        );
-                      },
-                      suggestionBuilder: (context, state, profile) {
-                        return ListTile(
-                          key: ObjectKey(profile),
-                          leading: CircleAvatar(
-                            backgroundImage: NetworkImage(profile.imageUrl),
-                          ),
-                          title: Text(profile.name),
-                          subtitle: Text(profile.email),
-                          onTap: () => state.selectSuggestion(profile),
-                        );
-                      },
-                    ),
+                    // FormBuilderChipsInput(
+                    //   decoration: InputDecoration(labelText: "Chips"),
+                    //   attribute: 'chips_test',
+                    //   onChanged: _onChanged,
+                    //   initialValue: [
+                    //     Contact('Andrew', 'stock@man.com',
+                    //         'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4057996.jpg'),
+                    //   ],
+                    //   maxChips: 5,
+                    //   findSuggestions: (String query) {
+                    //     if (query.length != 0) {
+                    //       var lowercaseQuery = query.toLowerCase();
+                    //       return contacts.where((profile) {
+                    //         return profile.name.toLowerCase().contains(query.toLowerCase()) ||
+                    //             profile.email.toLowerCase().contains(query.toLowerCase());
+                    //       }).toList(growable: false)
+                    //         ..sort((a, b) => a.name
+                    //             .toLowerCase()
+                    //             .indexOf(lowercaseQuery)
+                    //             .compareTo(b.name.toLowerCase().indexOf(lowercaseQuery)));
+                    //     } else {
+                    //       return const <Contact>[];
+                    //     }
+                    //   },
+                    //   chipBuilder: (context, state, profile) {
+                    //     return InputChip(
+                    //       key: ObjectKey(profile),
+                    //       label: Text(profile.name),
+                    //       avatar: CircleAvatar(
+                    //         backgroundImage: NetworkImage(profile.imageUrl),
+                    //       ),
+                    //       onDeleted: () => state.deleteChip(profile),
+                    //       materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    //     );
+                    //   },
+                    //   suggestionBuilder: (context, state, profile) {
+                    //     return ListTile(
+                    //       key: ObjectKey(profile),
+                    //       leading: CircleAvatar(
+                    //         backgroundImage: NetworkImage(profile.imageUrl),
+                    //       ),
+                    //       title: Text(profile.name),
+                    //       subtitle: Text(profile.email),
+                    //       onTap: () => state.selectSuggestion(profile),
+                    //     );
+                    //   },
+                    // ),
                     FormBuilderDateTimePicker(
                       attribute: "date",
                       onChanged: _onChanged,
