235fd1de7da18eb675b2052551bf430cefb1e826
Alek Slater <skela@davincium.com>
2022-03-15 14:25:18 +0100

    cleaned up some warnings, bumped touch spin, and intl

--- lib/src/fields/form_builder_switch.dart
+++ lib/src/fields/form_builder_switch.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 class FormBuilderSwitch extends StatefulWidget {
